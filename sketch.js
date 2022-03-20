858447abc5a4cc1afefb697fe7f8275e7c5102fe
Maluquin <93450411+Maluquin@users.noreply.github.com>
2022-03-20 16:09:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var notas = [35,38,42,45,43,34,36,41,48,32];
+var soma = 0;
+
+
+
+function setup() {
+  createCanvas(400,400);
+ for (var i = 0; i< notas.length; i++){
+   if ( notas[i] > 40){
+   console.log( notas [i]);
+  //soma += notas[i];
+   }
+}
+
+//var media = soma/notas.length;
+//console.log (media);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
